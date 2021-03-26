76bbd0904d187b49de2584f4313b2283dce1fb03
Kiran6248 <kiran.satyarthy@gmail.com>
2021-03-26 16:12:24 +0000

    Add Footer

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
   });
+
+// -------------------------------------------------------------------- footer copyright
+function copyrightYear() {
+    var d = new Date();
+    var y = d.getFullYear();
+    document.getElementById("copyright").innerHTML = y;
+}
+copyrightYear();
