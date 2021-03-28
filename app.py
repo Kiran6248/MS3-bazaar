e62a708f6070e60a882faa8d4bab35cf5f137ec6
Kiran6248 <kiran.satyarthy@gmail.com>
2021-03-28 14:58:57 +0000

    Create post_ad template

--- app.py
+++ app.py
@@ -105,6 +105,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/post_ad")
+def post_ad():
+    return render_template("post_ad.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
